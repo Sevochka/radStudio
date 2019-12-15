ae9121b27e335e88e815471be9bd3627045fe3dc
Sevka <sevkaplay@gmail.com>
2019-12-15 22:13:18 +0300

    names

---
+++ homeWork2/toDoList.cpp
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+#pragma hdrstop
+#include <System.StartUpCopy.hpp>
+//---------------------------------------------------------------------------
+USEFORM("toDoListUnit.cpp", TabbedwithNavigationForm);
+USEFORM("dmu.cpp", dm);
+//---------------------------------------------------------------------------
+extern "C" int FMXmain()
+{
+	try
+	{
+		Application->Initialize();
+		Application->CreateForm(__classid(TTabbedwithNavigationForm), &TabbedwithNavigationForm);
+		Application->CreateForm(__classid(Tdm), &dm);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
